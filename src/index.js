4bb4a4285109eeea7c14b3f2a1250798fe3482af
rutkowsm <37616390+rutkowsm@users.noreply.github.com>
2023-05-30 00:18:40 +0200

    1 file modified

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ const express = require('express');
 const app = express();
 const port = 9393;
 
-app.get('/', (req, res) => {
-  res.send('Hello from the server!');
-});
+const App = () => (
+  <html lang="pl">
+    <head>
+      <meta charset="utf-8" />
+      <title>templatka</title>
+      <meta name="description" content="temlate" />
+      <meta name="author" content="SitePoint" />
+      <link rel="stylesheet" href="css/styles.css?v=1.0" />
+    </head>
+    <body>
+      <h1>Lorem</h1>
+    </body>
+  </html>
+);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
